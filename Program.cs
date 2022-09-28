ad54386b2894835ddecfb8b8d3c70a8959e31f41
«Ekaterina <ezhova.kate@mail.ru>
2022-09-28 16:47:32 +0300

    Первый вариант решения в цифровом массиве

--- Program.cs
+++ Program.cs
@@ -9,3 +9,27 @@
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → [] */
+
+Console.Clear();
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[length];
+Console.WriteLine("Полученный массив: ");
+for (int i = 0; i < length; i++)
+{
+    arr[i] = new Random().Next(10, 3000);
+    Console.Write($"{arr[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив: ");
+for (int i = 0; i < length; i++)
+{
+    if (arr[i] <= 999)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    else
+    {
+        arr[i] = arr[i];
+    }
+}
